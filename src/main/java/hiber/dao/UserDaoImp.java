75c3ffe3f96ab1e9e72224c34a7a200e09b9f0cf
borisat <118672475+borisat@users.noreply.github.com>
2023-05-05 17:41:24 +0300

    Initial commit

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,47 @@
+package hiber.dao;
+
+import hiber.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+   @Autowired
+   private SessionFactory sessionFactory;
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsers() {
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
+
+
+
+   @Override
+   public User getUserByCar(String model, int series) {
+
+      String hql = "SELECT u FROM User u JOIN u.car c WHERE c.model = :model AND c.series = :series";
+      Query query = sessionFactory.openSession().createQuery(hql);
+      query.setParameter("model", model);
+      query.setParameter("series", series);
+      query.setMaxResults(1);
+      User user = (User) query.getSingleResult();
+      return user;
+   }
+
+}
---
+++ src/main/java/hiber/service/UserServiceImp.java
@@ -0,0 +1,45 @@
+package hiber.service;
+
+import hiber.dao.UserDao;
+import hiber.dao.UserDaoImp;
+import hiber.model.Car;
+import hiber.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.Calendar;
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Transactional
+    @Override
+    public void add(User user) {
+        userDao.add(user);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<User> listUsers() {
+        return userDao.listUsers();
+    }
+
+    @Transactional(readOnly = true)
+    public List<User> listUsersByCar(Car car) {
+        return userDao.listUsers();
+    }
+
+    public User getUserByCar(String model, int series) {
+
+        return userDao.getUserByCar(model, series);
+    }
+
+}
